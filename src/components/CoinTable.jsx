dac88c407e5ec1066f6599e37f92384b50059c3d
Pablo Marino <pmarino2013@gmail.com>
2022-03-14 12:39:14 -0300

    Componente CoinTable

---
+++ src/components/CoinTable.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import numeral from "numeral";
+import CoinIcon from "./CoinIcon";
+
+const CoinTable = ({ update, setUpdate, datos }) => {
+  return (
+    <table className="table table-coin">
+      <thead>
+        <tr>
+          <th>#Rank</th>
+          <th>Name</th>
+          <th>Price Usd</th>
+          <th>Market Cap</th>
+          <th>VWAP(24hs)</th>
+          <th>Supply</th>
+          <th>Volume(24hs)</th>
+          <th>Change(24hs)</th>
+          <th>
+            <button className="btn btn-coin" onClick={() => setUpdate(!update)}>
+              Update
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {datos.map((coin) => (
+          <tr key={coin.id}>
+            <td className="text-center">{coin.rank}</td>
+            <td className="d-flex justify-content-between">
+              <CoinIcon symbol={coin.symbol} />
+              {coin.name}
+            </td>
+            <td>{numeral(coin.priceUsd).format("$0,0.00")}</td>
+            <td>{numeral(coin.marketCapUsd).format("($ 0.00 a)")}</td>
+            <td>{numeral(coin.vwap24Hr).format("$0,0.00")}</td>
+            <td>{numeral(coin.supply).format("($ 0.00 a)")}</td>
+            <td>{numeral(coin.volumeUsd24Hr).format("($ 0.00 a)")}</td>
+            <td
+              className={
+                coin.changePercent24Hr > 0 ? "text-success" : "text-danger"
+              }
+            >
+              {parseFloat(coin.changePercent24Hr).toFixed(2)}%
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default CoinTable;
